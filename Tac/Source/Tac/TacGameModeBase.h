9e38521f01459d4dbf12a9bc7860b7cb6109699f
Alex Trevor <pascerveau@gmail.com>
2017-02-12 17:14:25 +0800

    Initial project.

---
+++ Tac/Source/Tac/TacGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TacGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAC_API ATacGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
